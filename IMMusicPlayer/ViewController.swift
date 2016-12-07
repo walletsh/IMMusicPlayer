804de578064cb6c3f165e12795e7ff5650bcc95a
LeeWorker <lsh249410504@163.com>
2016-12-07 17:05:30 +0800

    Initial Commit

---
+++ IMMusicPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IMMusicPlayer
+//
+//  Created by imwallet on 16/12/7.
+//  Copyright © 2016年 imWallet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
